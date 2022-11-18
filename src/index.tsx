362f4133db949dc8a83f4164636ec520d6eebbe5
SuKyoung Shin <sukyoung.dev@gmail.com>
2022-11-19 01:50:28 +0900

    🎉 프로젝트 기본 셋팅

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,30 @@
 import * as React from "react";
-import * as ReactDOM from "react-dom";
+import * as ReactDOM from "react-dom/client";
+import { createGlobalStyle } from "styled-components";
 import App from "./App";
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+  }
+  html, 
+  body {
+    margin: 0;
+    padding: 0;
+  }
+  html, 
+  body,
+  #root {
+    width: 100%;
+    height: 100%;
+  }
+`;
 
 // @ts-ignore
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
+    <GlobalStyle />
   </React.StrictMode>
 );
